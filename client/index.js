e245cb4390af470f68ef6f539434d85f9028b7b1
Mustafa Dane <mustafadane@gmail.com>
2019-02-07 14:51:22 -0600

    hashrouter

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import App from './App'
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
     <Router>
