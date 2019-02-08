d401d7f11f0d2325136cf926434815f1fa93ffa4
Mustafa Dane <mustafadane@gmail.com>
2019-02-08 09:29:32 -0600

    commit without bundle

--- client/App.js
+++ client/App.js
@@ -7,10 +7,10 @@ export class App extends Component {
     render() {
         return (
             <div>
-                <div>REACT IS WORKING</div>
                 <Switch>
                     <Route path='/login' component={Login} />
-                    <Route path='/' component={Home} />
+                    <Route path='/home' component={Home} />
+                    <Route component={Login} />
                 </Switch>
             </div>
         )
