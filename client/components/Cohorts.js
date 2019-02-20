4a17fc05629a720b60249d658aa31070aa2d9489
Mustafa Dane <mustafadane@gmail.com>
2019-02-20 11:16:03 -0600

    pre webpack dev server

---
+++ client/components/Cohorts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Cohorts extends Component {
+    render() {
+        return (
+            <div>This is Cohorts</div>
+        )
+    }
+}
+
+export default Cohorts
---
+++ client/components/Playbooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Playbooks extends Component {
+    render() {
+        return (
+            <div>This is playbooks</div>
+        )
+    }
+}
+
+export default Playbooks
